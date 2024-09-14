d97b2141288b9e26b73cd719a93b6ae3231a8ef0
Michał Olkiewicz <m.j.olkiewicz@gmail.com>
2024-09-14 23:40:53 +0200

    Jump buffer added and weird jump isntant on landing fixed

--- Game/player_character/player_jump.gd
+++ Game/player_character/player_jump.gd
@@ -9,6 +9,9 @@ signal pressed_jump
 @export_range(0.0, 100.0) var jump_height : float = 45.0
 @export_range(0,1) var max_air_jumps : int = 0
 
+@export_category("Options")
+@export_range(0.0, 0.3) var jump_buffer : float = 0.15
+
 @export_group("References")
 #@export var ground : PlayerGroundCheck
 @export var character_body_2d : CharacterBody2D
@@ -20,6 +23,7 @@ var desired_jump : bool
 var pressing_jump : bool
 var on_ground : bool
 var can_jump_again : bool = false
+var jump_buffer_counter : float
 #endregion
 
 func _input(event: InputEvent) -> void:
@@ -33,6 +37,14 @@ func _input(event: InputEvent) -> void:
 func _process(delta: float) -> void:
 	on_ground = character_body_2d.is_on_floor()
 	
+	if jump_buffer > 0:
+		if desired_jump:
+			jump_buffer_counter += delta
+			
+			if jump_buffer_counter > jump_buffer:
+				desired_jump = false
+				jump_buffer_counter = 0
+	
 func _physics_process(delta: float) -> void:
 	if not on_ground:
 		character_body_2d.velocity += character_body_2d.get_gravity() * delta * gravity_scale
@@ -42,9 +54,15 @@ func _physics_process(delta: float) -> void:
 
 
 func do_a_jump() -> void:
-	
+	print("Doing a jump")
+	print("On Ground "+str(on_ground))
+	print("Can jump again "+str(can_jump_again))
 	if on_ground or can_jump_again:
 		desired_jump = false
+		jump_buffer_counter = 0
 		can_jump_again = (max_air_jumps == 1 and can_jump_again == false)
 		
 		character_body_2d.velocity.y = -(jump_height * 10)
+		
+	if jump_buffer == 0:
+		desired_jump = false
