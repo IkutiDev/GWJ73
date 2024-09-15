07a5f3a81a20ff56b709b96a02c7dc6dbe3ed496
Ktosox <ktoso3@gmail.com>
2024-09-15 15:08:33 +0200

    made a cool little jumping spring
    currently only works with puppet

---
+++ Game/experimental/jumpy_spring.gd
@@ -0,0 +1,26 @@
+extends StaticBody2D
+
+var player : CharacterBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func apply_jump_boost(power : float):
+	player.jump_boost = power
+	pass
+
+func _on_player_detector_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player = body
+		$CoilAniamtor.play("coiling")
+	pass # Replace with function body.
+
+
+func _on_player_detector_body_exited(body: Node2D) -> void:
+	$CoilAniamtor.play("RESET")
+	apply_jump_boost(0.0)
+	pass # Replace with function body.
--- Game/experimental/puppet.gd
+++ Game/experimental/puppet.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = -400.0
 
 var is_dragged = false
 
+var jump_boost = 0.0
+
 func _physics_process(delta: float) -> void:
 	
 	if is_dragged:
@@ -19,7 +21,7 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY + jump_boost * JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
