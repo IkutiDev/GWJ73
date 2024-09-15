2aa0ed99bdcfa4a6fd969d043931a8f2583dc5aa
Michał Olkiewicz <m.j.olkiewicz@gmail.com>
2024-09-16 00:32:05 +0200

    Added in death animation to the test scene

--- Game/experimental/death_sentence.gd
+++ Game/experimental/death_sentence.gd
@@ -5,6 +5,14 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		var children = body.get_children()
+		
+		for c in children:
+			var player_visual : PlayerVisual = c as PlayerVisual
+			if player_visual != null:
+				player_visual.death()
+				
+		await get_tree().create_timer(1.0).timeout
 		print("kill player")
 		body.queue_free()
 	pass # Replace with function body.
