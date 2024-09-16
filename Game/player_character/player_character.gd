d9e026c56cd9198868d91d912aec1a24028433f1
Michał Olkiewicz <m.j.olkiewicz@gmail.com>
2024-09-16 23:56:35 +0200

    Simplify ways to access player jump/movement for other scripts

---
+++ Game/player_character/player_character.gd
@@ -0,0 +1,5 @@
+class_name PlayerCharacter
+extends CharacterBody2D
+# Use this script as an easy way to interact with deeper components
+@export var player_jump : PlayerJump
+@export var player_move : PlayerMovement
