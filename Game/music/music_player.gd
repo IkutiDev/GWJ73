d07a897cf85faafea1d226408736338b0512546c
Ktosox <ktoso3@gmail.com>
2024-09-22 11:35:02 +0200

    added death and victory SFX
    allowed for music to be tuned dow nwhile other SFX are played

--- Game/music/music_player.gd
+++ Game/music/music_player.gd
@@ -6,16 +6,26 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if not OS.has_feature("editor"):
-		background_music_player.volume_db = 0
+		AudioServer.set_bus_mute(1,false)
+		#background_music_player.volume_db = 0
 	else:
-		background_music_player.volume_db = -80 if mute_in_editor else 0
+		AudioServer.set_bus_mute(1,true) if mute_in_editor else 0
 
 
+func play_death():
+	if $Death.playing:
+		return
+	var music_position = $Music.get_playback_position()
+	print(music_position)
+	$Music.stop()
+	$Music.volume_db = -80
+	$Death.play()
+	await get_tree().create_timer(1.8).timeout
+	$Music.play(music_position)
+	var sound_tween = get_tree().create_tween()
+	sound_tween.tween_property($Music,"volume_db",0,1.0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	pass
-
 
 func _on_music_finished() -> void:
 	$Music.stream = load("res://music/A Quest For A Hero Music_Loop.mp3")
