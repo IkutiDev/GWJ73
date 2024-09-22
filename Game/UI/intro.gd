7706eea337fc615e026638722b835949f2c89597
Ktosox <ktoso3@gmail.com>
2024-09-22 20:21:17 +0200

    slight correction to audio mute in intro

--- Game/UI/intro.gd
+++ Game/UI/intro.gd
@@ -7,10 +7,13 @@ signal intro_finished
 
 
 func _enter_tree() -> void:
-	AudioServer.set_bus_mute(1,true)
+	AudioServer.set_bus_mute(0,true)
 
 func _process(delta: float) -> void:
 	if video_player.stream_position >= 8.0 and not video_player.paused:
 		video_player.paused = true
-		AudioServer.set_bus_mute(1,false)
+		
 		intro_finished.emit()
+
+func _exit_tree() -> void:
+	AudioServer.set_bus_mute(0,false)
